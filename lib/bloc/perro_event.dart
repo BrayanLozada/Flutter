657d5c087c6cd704504fb6a3c24d81d4e2fa6a1f
Brayan Lozada <95394978+BrayanLozada@users.noreply.github.com>
2023-05-03 21:42:38 -0500

    Eliminar directorio redundante

--- lib/bloc/perro_event.dart
+++
@@ -1,9 +0,0 @@
-part of 'perro_bloc.dart';
-
-@immutable
-abstract class PerrosEvent {}
-
-class PerroAgregado extends PerrosEvent {
-  final Perro perro;
-  PerroAgregado({required this.perro});
-}
